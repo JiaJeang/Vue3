b3da09766151711679b14f823236d5c9acc096bb
Jeang <2609231566@qq.com>
2026-05-23 18:52:07 +0800

    props,emit

---
+++ 02 props,emit/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from "vue";
+import son from "./components/icons/son-com.vue";
+/*
+  一. 父传子
+      1. 给子组件添加属性的方式传值
+      2. 在子组件通过props接收
+  二. 子传父
+      1. 在子组件内部 emit 触发事件(编译器宏获取)
+      2. 在父组件通过 @ 监听
+*/
+const count = ref(20);
+const name = ref("Jeang");
+const addCount = (n) => (count.value += n);
+const subCount = (n) => (count.value -= n);
+</script>
+
+<template>
+  <h1>
+    父组件-{{ count }}
+    <button @click="addCount(10)">+10</button>
+  </h1>
+  <!-- 渲染组件不必要注册 传递方式与Vue2一样 -->
+  <son :name="name" :count="count" @subCount="subCount"></son>
+</template>
---
+++ 02 props,emit/components/icons/son-com.vue
@@ -0,0 +1,27 @@
+<script setup>
+// 由于写了 setup 所以无法直接配置 props 选项
+// 所以需要借助于"编译器宏"函数接收子组件传递的数据
+const props = defineProps({
+  name: String,
+  count: Number,
+});
+// 注册事件
+const emit = defineEmits(['subCount'])
+// 通过 emit 触发注册的事件
+const sub=(n)=>{emit('subCount',n)}
+</script>
+
+<template>
+  <div class="box">
+    {{ name }}- {{ count }}-
+    <button @click="sub(10)">-10</button>
+  </div>
+</template>
+
+<style scoped lang="less">
+.box {
+  height: 200px;
+  width: 200px;
+  border: 1px solid #000;
+}
+</style>
